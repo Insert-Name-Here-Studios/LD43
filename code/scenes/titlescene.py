4dbdda5f57cdeee31090a996e9ff98ab96e3441e
TheFoppian <38819438+TheFoppian@users.noreply.github.com>
2018-12-01 00:22:07 -0600

    added a scrolling text method

--- code/scenes/titlescene.py
+++ code/scenes/titlescene.py
@@ -1,14 +1,26 @@
 from scenebase import sceneBase
+from utils import scrolling_text
+import pygame
 
 class TitleScene(sceneBase):
 	def __init__(self):
 		sceneBase.__init__(self)
+		self.font = pygame.font.SysFont('arial', 24)
+		self.delay = 1000
+		pygame.time.set_timer(pygame.USEREVENT, 500)
+		self.textbox = scrolling_text.DynamicText(self.font, 'test sentence for the boys', (500, 200))
 
 	def update(self):
+		#pygame.time.set_timer(pygame.USEREVENT, self.delay)
 		pass
 
 	def processInput(self, events, pressed_keys):
-		pass
+		for event in events:
+			if event.type == pygame.USEREVENT:
+				self.textbox.update()
+
+
 
 	def render(self, screen):
 		screen.fill((255, 0, 0))
+		self.textbox.draw(screen)
---
+++ code/scenes/utils/scrolling_text.py
@@ -0,0 +1,37 @@
+import pygame
+
+def text_generator(text):
+	tmp = ''
+	for letter in text:
+		tmp += letter
+		# don't pause for spaces
+		if letter != ' ':
+			yield tmp
+# a simple class that uses the generator
+# and can tell if it is done
+class DynamicText(object):
+    def __init__(self, font, text, pos, autoreset=False):
+        self.done = False
+        self.font = font
+        self.text = text
+        self._gen = text_generator(self.text)
+        self.pos = pos
+        self.autoreset = autoreset
+        self.update()
+
+
+
+    def reset(self):
+        self._gen = text_generator(self.text)
+        self.done = False
+        self.update()
+
+    def update(self):
+        if not self.done:
+            try: self.rendered = self.font.render(next(self._gen), True, (0, 128, 0))
+            except StopIteration:
+                self.done = True
+                if self.autoreset: self.reset()
+
+    def draw(self, screen):
+        screen.blit(self.rendered, self.pos)
