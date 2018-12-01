7ad3e3d319685e97dc648ed75aabe20b9dabfc34
TheFoppian <38819438+TheFoppian@users.noreply.github.com>
2018-12-01 11:57:58 -0600

    added a day and to title scene

---
+++ code/scenes/day1.py
@@ -0,0 +1,14 @@
+import pygame
+from scenes.scenebase import sceneBase
+class day1(sceneBase):
+	def __init__(self):
+		sceneBase.__init__(self)
+
+	def processInput(self, events, pressed_keys):
+		pass
+
+	def update(self):
+		pass
+
+	def render(self, screen):
+		screen.fill(255, 255, 0)
--- code/scenes/titlescene.py
+++ code/scenes/titlescene.py
@@ -1,5 +1,7 @@
 from scenes.scenebase import sceneBase
 from scenes.utils import scrolling_text
+from scenes.utils.button import button
+from scenes.day1 import day1
 import pygame
 
 class TitleScene(sceneBase):
@@ -10,11 +12,14 @@ class TitleScene(sceneBase):
 		self.delay = 1000
 		self.titletext = 'TITLE (TODO: CREATE TITLE)'
 		self.title = self.font.render(self.titletext, False, (0, 0, 0))
+		self.startbutton = button(400, 100, 0, 0, '../../art/startmenu/start button.png')
 
 
 	def update(self):
 		#pygame.time.set_timer(pygame.USEREVENT, self.delay)
-		pass
+		self.startbutton.update()
+		if self.startbutton.Lclicked:
+			self.switchScene(day1())
 
 	def processInput(self, events, pressed_keys):
 		pass
@@ -22,4 +27,5 @@ class TitleScene(sceneBase):
 
 	def render(self, screen):
 		screen.fill((255, 255, 255))
+		screen.blit(self.startbutton.img, (screen.get_width() / 2 - self.startbutton.img.get_width() / 2, screen.get_height() / 2 - self.startbutton.img.get_height() / 2))
 		screen.blit(self.title, (screen.get_width() / 2 - self.font.size(self.titletext)[0] / 2, screen.get_height() / 2 - self.font.size(self.titletext)[1] / 2 ))
