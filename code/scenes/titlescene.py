a3f4ecd3ef1827b09eeaeab13943721a3264b082
TheFoppian <38819438+TheFoppian@users.noreply.github.com>
2018-12-01 08:51:26 -0600

    fixed text scrolling

--- code/scenes/titlescene.py
+++ code/scenes/titlescene.py
@@ -7,8 +7,8 @@ class TitleScene(sceneBase):
 		sceneBase.__init__(self)
 		self.font = pygame.font.SysFont('arial', 24)
 		self.delay = 1000
-		pygame.time.set_timer(pygame.USEREVENT, 500)
-		self.textbox = scrolling_text.DynamicText(self.font, 'test sentence for the boys', (500, 200))
+		pygame.time.set_timer(pygame.USEREVENT, 200)
+		self.textbox = scrolling_text.DynamicText(self.font, ['test sentence for ^the boys', 'Yeeeeeeee^eeeeeeet'], (500, 200), (0, 0, 0))
 
 	def update(self):
 		#pygame.time.set_timer(pygame.USEREVENT, self.delay)
@@ -22,5 +22,5 @@ class TitleScene(sceneBase):
 
 
 	def render(self, screen):
-		screen.fill((255, 0, 0))
+		screen.fill((255, 255, 255))
 		self.textbox.draw(screen)
--- code/scenes/utils/scrolling_text.py
+++ code/scenes/utils/scrolling_text.py
@@ -1,23 +1,27 @@
 import pygame
 
-def text_generator(text):
-	tmp = ''
-	for letter in text:
-		tmp += letter
-		# don't pause for spaces
-		if letter != ' ':
-			yield tmp
+def text_generator(texts):
+	for text in texts:
+		lines = text.split('^')
+		for line in lines:
+			tmp = ''
+			for letter in line:
+				tmp += letter
+				# don't pause for spaces
+				if letter != ' ':
+					yield tmp
 # a simple class that uses the generator
 # and can tell if it is done
 class DynamicText(object):
-    def __init__(self, font, text, pos, autoreset=False):
-        self.done = False
-        self.font = font
-        self.text = text
-        self._gen = text_generator(self.text)
-        self.pos = pos
-        self.autoreset = autoreset
-        self.update()
+    def __init__(self, font, text, pos, color=(0, 0, 0), autoreset=False):
+		self.done = False
+		self.font = font
+		self.text = text
+		self._gen = text_generator(self.text)
+		self.pos = pos
+		self.color = color
+		self.autoreset = autoreset
+		self.update()
 
 
 
@@ -28,7 +32,7 @@ class DynamicText(object):
 
     def update(self):
         if not self.done:
-            try: self.rendered = self.font.render(next(self._gen), True, (0, 128, 0))
+            try: self.rendered = self.font.render(next(self._gen), True, self.color)
             except StopIteration:
                 self.done = True
                 if self.autoreset: self.reset()
