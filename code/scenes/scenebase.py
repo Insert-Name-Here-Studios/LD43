34eb58f90ac53aa3482ae979f35d901dffb5793e
TheFoppian <38819438+TheFoppian@users.noreply.github.com>
2018-11-30 23:05:18 -0600

    scene base created

---
+++ code/scenes/scenebase.py
@@ -0,0 +1,19 @@
+class sceneBase:
+	def __init__(self):
+		pygame.font.init()
+		self.next = self
+
+	def processInput(self, events, pressed_keys):
+		print('Not overridden in chile class!')
+
+	def update(self):
+		print('Not overridden in chile class!')
+
+	def render(self, screen):
+		print('Not overridden in chile class!')
+
+	def switchScene(self, next_scene):
+		self.next = next_scene
+
+	def terminate(self):
+		self.switchScene(None)
