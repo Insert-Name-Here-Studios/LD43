7fab608f096f85f13ea9c5bf980e75f3d3341c81
ansaws <junk.ansaws@gmail.com>
2020-07-15 11:30:34 -0500

    something

--- code/scenes/utils/textbox.py
+++ code/scenes/utils/textbox.py
@@ -5,20 +5,20 @@ import pygame
 
 class TextBox:
     def __init__(self, dynatext, path, delay, w, h):
-		self.textbox_x = dynatext.pos[0] - 10
-		self.textbox_y = dynatext.pos[1] - 5
+        self.textbox_x = dynatext.pos[0] - 10
+        self.textbox_y = dynatext.pos[1] - 5
 
-		try:
-		    self.img = pygame.image.load(path)
-		except:
-			print('error loading image')
+        try:
+            self.img = pygame.image.load(path)
+        except:
+        	print('error loading image')
 
-		self.img = pygame.transform.scale(self.img, (w, h))
-		self.dynatext = dynatext
+        self.img = pygame.transform.scale(self.img, (w, h))
+        self.dynatext = dynatext
 
-		self.rect = pygame.Rect(self.textbox_x, self.textbox_y, self.img.get_width(), self.img.get_height())
+        self.rect = pygame.Rect(self.textbox_x, self.textbox_y, self.img.get_width(), self.img.get_height())
 
-		pygame.time.set_timer(pygame.USEREVENT, delay)
+        pygame.time.set_timer(pygame.USEREVENT, delay)
 
     def update(self):
         for event in pygame.event.get():
